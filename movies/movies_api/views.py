9e6229598d097beff097d693a6bac578644f18fb
amideadagain <48013675+amideadagain@users.noreply.github.com>
2022-01-18 02:47:39 +0200

    api pagination&filters

--- movies/movies_api/views.py
+++ movies/movies_api/views.py
@@ -1,10 +1,6 @@
-from rest_framework.pagination import PageNumberPagination
-# from rest_framework.views import APIView
 from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
-# from rest_framework import authentication, permissions
 from rest_framework import viewsets
-from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import action
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
@@ -24,49 +20,20 @@ class MovieViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAuthenticated | ReadOnly]
+    filterset_fields = ['title', 'year']
+    ordering_fields = ['year', 'title']
+    ordering = ['-year']
 
-    def paginate_movies_api(self, request, queryset):
-        paginator = PageNumberPagination()
-        page = paginator.paginate_queryset(queryset, request)
-
-        if page is not None:
-            serializer = MovieSerializer(page, many=True)
-            return paginator.get_paginated_response(serializer.data)
-        else:
-            serializer = MovieSerializer(queryset, many=True)
-            return Response(serializer.data)
-
-    @method_decorator(cache_page(60*60))
-    def list(self, request):
-        queryset = Movie.objects.all()
-        return self.paginate_movies_api(request, queryset)
-
-    def create(self, request):
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-
-    @method_decorator(cache_page(60 * 60))
-    def retrieve(self, request, pk=None):
-        queryset = Movie.objects.api_get(pk)
-
-        serializer = MovieSerializer(queryset)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        queryset = Movie.objects.api_get(pk)
-
-        serializer = MovieSerializer(queryset, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(serializer.data)
+    @method_decorator(cache_page(60 * 2))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
     @method_decorator(cache_page(60 * 60))
     @action(methods=['get'], detail=False, name='Top movies')
     def top(self, request, limit=10):
         queryset = Movie.objects.top_movies(limit=limit)
-        return self.paginate_movies_api(request, queryset)
+        serializer = MovieSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class VoteViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
